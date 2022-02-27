713373d045e4c620ed90b9fbcb4cd901db70d957
NotTimIsReal <alastairrintumathew@gmail.com>
2022-02-27 14:03:16 +1100

    Add .env support fir ports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { config } from 'dotenv';
+config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+  });
+  await app.listen(process.env.PORT || 3000).then(() => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
